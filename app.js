6ad9c29e66e11bd9ea78b2d780edd6a0870536f5
Fortunat Mutunda <fotimutunda@gmail.com>
2016-05-01 01:59:09 +0300

    reroute

--- app.js
+++ app.js
@@ -4,76 +4,32 @@
 
 var express = require('express'),
     mongoose = require('mongoose'),
-    bodyParse = require('body-parser');
+    bodyParser = require('body-parser');
+
 
 var db = mongoose.connect('mongodb://localhost/bookAPI');
-var Book = require('./Models/bookModel');
+
+var Book = require('./models/bookModel');
 
 var app = express();
 
 var port = process.env.PORT || 3000;
-app.use(bodyParse.urlencoded({extended:true}));
-app.use(bodyParse.json());
 
-var bookRouter = express.Router();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
-bookRouter.route('/Books')
-    .post(function (req,res) {
-        var book = new Book(req.body);
-        book.save();
-        res.status(201).send(book);
-        
-    })
-    
-    .get(function (req, res) {
+bookRouter = require('./Routes/bookRoutes')(Book);
 
-        var query = {};
 
-        if( req.query.genre){
+app.use('/api/books', bookRouter);
 
-            query.genre = req.query.genre
-        }
 
-        Book.find(function (err,books) {
-            
-            if(err){
-                res.status(500).send(err);
-            }else {
-                res.json(books);
-            }
-        })
+app.get('/', function(req, res){
+    res.send('welcome to my API!');
 });
 
-bookRouter.route('/Books/:bookId')
-    .get(function (req, res) {
-
-        Book.findById(req.params.bookId,function (err,books) {
-
-            if(err){
-                res.status(500).send(err);
-            }else {
-                res.json(books);
-            }
-        })
-    }
-);
-
-app.use('/api',bookRouter);
-
-
-
-
-app.get('/',function (req,res) {
-    res.send("Welcome to my API SIR");
-
+app.listen(port, function(){
+    console.log('Gulp is running my app on  PORT: ' + port);
 });
-
-
-app.listen(port,function () {
-
-    console.log('gulp running on localhost:'+ port);
-
-});
-
 
 
